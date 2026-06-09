9a7097f31944d51225c734d2395dc58a944b599a
risibaba <rishibankar16@gmail.com>
2026-06-09 22:44:15 +0530

    register and login done

--- server/src/controller/authController.js
+++ server/src/controller/authController.js
@@ -11,20 +11,44 @@ const register = async(req,res)=>{
     
     const { firstName, emailId, lastName, age, password  } = req.body
 
-    const user = {
-      firstName,
-      lastName,
-      email,
-      age,
-    }
+    
+    req.body.password = await bcrypt.hash(password, 10 )
+    
+    const user = await User.create(req.body)
+    
+    const token =  jwt.sign({userId:user._id, email:emailId},process.env.JWT_TOKEN,{expiresIn:60*60})
 
-    bcrypt.hash(password, 10 )
-
-
+    res.cookie('token',token, {maxAge: 60 * 60 * 1000})
+    res.status(201).send("user created successfully")
 
 
   } catch (error) {
     console.log(error)
+    res.status(500).send("Something went wrong");
+  }
+
+}
+
+const login = async(req,res)=>{
+
+  try {
+    
+    const {emailId, password} = req.body
+    const user = User.findOne({emailId})
+
+    const isMatch = bcrypt.compare(user.password, password)
+    if(!ismMatch){
+      throw new Error("invalid cred")
+    }
+
+    const token = jwt.sign({userId:user._id, email:emailId},process.env.JWT_TOKEN,{expiresIn:60*60})
+
+    res.cookie('token',token, {maxAge: 60 * 60 * 1000})
+    res.status(201).send("user created successfully")
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).send("Something went wrong");
   }
 
 }
